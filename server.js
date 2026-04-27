0d9e0dbd5fb9fa4dd8fdf314f5afb2350d1a4d89
Sakthi Parthiban <sakthiparthibans@gmail.com>
2026-04-27 21:57:53 +0530

    add global and error middlewares

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const cors = require('cors');
+const helmet = require('helmet');
+
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
+
+// Global middleware
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 // MongoDB connection
@@ -13,7 +20,12 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.log("DB connection failed ❌"));
 
 app.use('/api', restaurantRoutes);
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
+
+// Error middlware
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message })
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
