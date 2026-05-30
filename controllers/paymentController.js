aa314b05a3c9bdda8decc652057ea3b5565de557
Sakthi Parthiban <sakthiparthibans@gmail.com>
2026-05-30 21:59:59 +0530

    Advanced razorpay features - verify payment, payment history

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,5 +1,8 @@
 const razorpay = require('../config/razorpay');
+const crypto = require('crypto');
+const Payment = require('../models/Payment');
 
+// create order
 const createOrder = async (req, res, next) => {
     try {
         const { amount } = req.body;
@@ -16,7 +19,7 @@ const createOrder = async (req, res, next) => {
         const order = await razorpay.orders.create(options);
         res.status(200).json({
             success: true,
-            options
+            order
         })
     }
     catch (err) {
@@ -24,4 +27,91 @@ const createOrder = async (req, res, next) => {
     }
 }
 
-module.exports = { createOrder };
+// payment verification
+const verifyPayment = async (req, res, next) => {
+    try {
+        const {
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature
+        } = req.body;
+
+        if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
+            const err = new Error("All feilds are required");
+            err.statusCode = 400;
+            return next(err);
+        }
+
+        // genrate signature
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+        const expectedSignature = crypto
+            .createHmac(
+                "sha256",
+                process.env.RAZORPAY_KEY_SECRET
+            )
+            .update(body)
+            .digest("hex");
+
+        // compare signature
+        if (expectedSignature !== razorpay_signature) {
+            const err = new Error("Payment verification failed");
+            err.statusCode = 400;
+            return next(err);
+        }
+
+        // Fetch order details
+        const order = await razorpay.orders.fetch(
+            razorpay_order_id
+        );
+
+        const existingPayment = await Payment.findOne({
+            paymentId: razorpay_payment_id
+        });
+
+        if (existingPayment) {
+            const err = new Error("Payment already recorded");
+            err.statusCode = 400;
+            return next(err);
+        }
+        
+        // save payment
+        const payment = await Payment.create({
+            userId: req.user.userId,
+            orderId: razorpay_order_id,
+            paymentId: razorpay_payment_id,
+            amount: order.amount / 100,
+            currency: order.currency,
+            status: 'success'
+        });
+        res.status(200).json({
+            success: true,
+            message: "Payment verified successfully",
+            payment
+        })
+    }
+    catch (err) {
+        next(err);
+    }
+}
+// payment history
+const getMyPayments = async (req, res, next) => {
+    try {
+
+        const payments = await Payment.find({
+            userId: req.user.userId
+        })
+            .sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            count: payments.length,
+            payments
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = { createOrder, verifyPayment, getMyPayments };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {createOrder} = require('../controllers/paymentController');
+const {createOrder,verifyPayment,getMyPayments} = require('../controllers/paymentController');
 const {verifyToken} = require('../middleware/authMiddleware');
 
 router.post('/create-order',verifyToken,createOrder);
+router.post('/verify-payment',verifyToken,verifyPayment);
+router.get('/history',verifyToken,getMyPayments);
 
 module.exports = router;
